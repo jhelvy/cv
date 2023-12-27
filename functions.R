75de5abeb556db9e1bf810381509ae8ecfc7cfdc
John Helveston <john.helveston@gmail.com>
2023-12-27 05:39:52 -0500

    init

---
+++ functions.R
@@ -0,0 +1,70 @@
+library(pander)
+library(stringr)
+library(dplyr)
+library(googlesheets4)
+library(lubridate)
+library(kableExtra)
+
+gs4_deauth()
+
+gscholar_stats <- function(url) {
+  cites <- get_cites(url)
+  return(paste(
+    'Citations:', cites$citations, '•',
+    'h-index:',   cites$hindex, '•',
+    'i10-index:', cites$i10index
+  ))
+}
+
+get_cites <- function(url) {
+  html <- xml2::read_html(url)
+  node <- rvest::html_nodes(html, xpath='//*[@id="gsc_rsb_st"]')
+  cites_df <- rvest::html_table(node)[[1]]
+  cites <- data.frame(t(as.data.frame(cites_df)[,2]))
+  names(cites) <- c('citations', 'hindex', 'i10index')
+  return(cites)
+}
+
+get_cv_sheet <- function(sheet) {
+    return(read_sheet(
+        ss = 'https://docs.google.com/spreadsheets/d/1xyzgW5h1rVkmtO1rduLsoNRF9vszwfFZPd72zrNmhmU/edit?usp=sharing',
+        sheet = sheet
+    ))
+}
+
+make_ordered_list <- function(x) {
+    return(pandoc.list(x, style = 'ordered'))
+}
+
+make_bullet_list <- function(x) {
+  return(pandoc.list(x, style = 'bullet'))
+}
+
+make_ordered_list_filtered <- function(df, cat) {
+  return(df %>%
+    filter(category == {{cat}}) %>%
+    pull(citation) %>%
+    make_ordered_list()
+  )
+}
+
+make_grants_list <- function(df, external, pi, affiliated) {
+    df$is_external <- df$external == external
+    df$is_pi <- df$pi == pi
+    df$is_affiliated <- df$affiliated == affiliated
+    x <- df %>%
+      filter(awarded == 1) %>%
+      filter(is_external) %>%
+      filter(is_pi) %>%
+      filter(is_affiliated) %>%
+      pull(citation)
+    return(make_ordered_list(x))
+}
+
+na_to_space <- function(x) {
+  return(ifelse(is.na(x), '', x))
+}
+
+enquote <- function(x) {
+  return(paste0('"', x, '"'))
+}
